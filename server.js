35207505b3cc60700f1030a2411f33da73d6ff0d
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-11-03 21:47:46 -0500

    finished MVP and Post Router

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const server = express();
+
+server.use(logger);
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+server.use((error, req, res, next) => {
+  console.log('Error!', error);
+})
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+function logger(req, res, next) {
+  const seconds = new Date().getSeconds();
 
-function logger(req, res, next) {}
+  if (seconds % 3 === 0) {
+    res.status(403).json({ message: 'api is down....  for now' });
+  } else {
+    next(0);
+  }
+}
 
 module.exports = server;
